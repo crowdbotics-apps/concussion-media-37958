87a0674e316b98248ce982970e3e9f1c84fbe04c
Crowdbotics <team@crowdbotics.com>
2022-12-20 21:58:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/concussion_media_37958/MainActivity.java
@@ -0,0 +1,15 @@
+package com.concussion_media_37958;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "concussion_media_37958";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "concussion_media_37958",
+  url: "https://concussion_media_37958.botics.co",
+  api: "https://concussion_media_37958.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
